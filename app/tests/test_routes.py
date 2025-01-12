cacf89e912bced5fb08acd32b5a4f80d09a425d5
Yrwing <yrwing.new@gmail.com>
2025-01-12 00:08:39 -0300

    Ajuste no arquivo de testes

--- app/tests/test_routes.py
+++ app/tests/test_routes.py
@@ -1,18 +1,23 @@
 import pytest
-from app import app
+from app import app, db
+from app.models import Item
 
 @pytest.fixture
 def client():
     """Fixture para configurar o cliente de teste do Flask."""
     app.config['TESTING'] = True
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///:memory:'  # Banco em memória
     with app.test_client() as client:
+        with app.app_context():
+            db.create_all()  # Cria as tabelas antes de cada teste
         yield client
 
 def test_home_route(client):
     """Testa a rota inicial '/'."""
     response = client.get('/')
     assert response.status_code == 200
-    assert b"Ola, Irvi!" in response.data
+    data = response.get_json()
+    assert data["message"] == "Olá, Irvi! Sua aplicação Python está rodando no Passenger!"
 
 def test_items_get_empty(client):
     """Testa a rota '/items' quando o banco está vazio."""
